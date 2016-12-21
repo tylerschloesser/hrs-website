38ed95f66e4973a81784aae3573aad370193a341
Tyler Schloesser <tylerschloesser@gmail.com>
2016-12-20 22:58:00 -0800

    save contact form

--- main.rb
+++ main.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'aws-sdk'
 
 set :bind, '0.0.0.0'
 
+Aws.config[:region] = 'us-west-2'
+
+ses_client = Aws::SES::Client.new
+
+puts ses_client.inspect
+
 get '/' do
   erb :index
+end
+
+post '/ajax/contact' do
+  now = Time.new
+  open('/var/hrs-website/contact.txt', 'a') do |f|
+    f << "#{request.ip} @ #{now.inspect}: #{params.inspect}\n"
+  end
 end
