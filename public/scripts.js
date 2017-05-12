935c6c8079c3da2f42d998462a46f9e5819913f9
Tyler Schloesser <tylerschloesser@gmail.com>
2017-05-12 12:27:45 -0700

    semantic ui integration

--- public/scripts.js
+++ public/scripts.js
@@ -10,4 +10,83 @@ $(document).ready(function() {
             alert('Message has been sent!');
         });
     });
+
+    $('#donate-button').click(function() {
+      $(this).addClass('loading');
+      $('#donate-form').submit();
+    });
+    
+    $('img[src="ev1.jpg"]').click(function() {
+      $('#ev1-modal').modal('show');
+    });
+    $('img[src="ev2.jpg"]').click(function() {
+      $('#ev2-modal').modal('show');
+    });
+    $('img[src="ev3.jpg"]').click(function() {
+      $('#ev3-modal').modal('show');
+    });
+    $('img[src="ev4.jpg"]').click(function() {
+      $('#ev4-modal').modal('show');
+    });
+
+    $('img[src="gs1.jpg"]').click(function() {
+      $('#gs1-modal').modal('show');
+    });
+    $('img[src="gs2.jpg"]').click(function() {
+      $('#gs2-modal').modal('show');
+    });
+    $('img[src="gs3.jpg"]').click(function() {
+      $('#gs3-modal').modal('show');
+    });
+    $('img[src="gs4.jpg"]').click(function() {
+      $('#gs4-modal').modal('show');
+    });
+    $('img[src="gs5.jpg"]').click(function() {
+      $('#gs5-modal').modal('show');
+    });
+    $('img[src="gs6.jpg"]').click(function() {
+      $('#gs6-modal').modal('show');
+    });
+    $('img[src="gs7.jpg"]').click(function() {
+      $('#gs7-modal').modal('show');
+    });
+    $('img[src="gs8.jpg"]').click(function() {
+      $('#gs8-modal').modal('show');
+    });
+
+    $('img[src="ws1.jpg"]').click(function() {
+      $('#ws1-modal').modal('show');
+    });
+    $('img[src="ws2.jpg"]').click(function() {
+      $('#ws2-modal').modal('show');
+    });
+
+    $('img[src="pc1.jpg"]').click(function() {
+      $('#pc1-modal').modal('show');
+    });
+    $('img[src="pc2.jpg"]').click(function() {
+      $('#pc2-modal').modal('show');
+    });
+    $('img[src="pc3.jpg"]').click(function() {
+      $('#pc3-modal').modal('show');
+    });
+    $('img[src="pc4.jpg"]').click(function() {
+      $('#pc4-modal').modal('show');
+    });
+
+    $('img[src="mr1.jpg"]').click(function() {
+      $('#mr1-modal').modal('show');
+    });
+    $('img[src="mr2.jpg"]').click(function() {
+      $('#mr2-modal').modal('show');
+    });
+    $('img[src="mr3.jpg"]').click(function() {
+      $('#mr3-modal').modal('show');
+    });
+    $('img[src="mr4.jpg"]').click(function() {
+      $('#mr4-modal').modal('show');
+    });
+    $('img[src="mr5.jpg"]').click(function() {
+      $('#mr5-modal').modal('show');
+    });
 });
