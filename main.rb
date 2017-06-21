bd1b282de86aa00dd310b4e9907040bff64b9331
Tyler Schloesser <tylerschloesser@gmail.com>
2017-06-20 23:52:46 -0700

    add basic email

--- main.rb
+++ main.rb
@@ -16,8 +16,42 @@ end
 
 post '/ajax/contact' do
   now = Time.new
+  htmlbody = "#{request.ip} @ #{now.inspect} <br> Name: #{params[:name]} <br> Email: #{params[:email]} <br> Message: #{params[:message]}\n"
+  textbody = "#{request.ip} @ #{now.inspect}: #{params.inspect}\n"
   open('/var/hrs-website/contact.txt', 'a') do |f|
-    f << "#{request.ip} @ #{now.inspect}: #{params.inspect}\n"
+    f << textbody
   end
+
+  begin
+    resp = ses_client.send_email({
+      destination: {
+        to_addresses: [
+          'tylerschloesser@gmail.com',
+        ],
+      },
+      message: {
+        body: {
+          html: {
+            charset: 'UTF-8',
+            data: htmlbody,
+          },
+          text: {
+            charset: 'UTF-8',
+            data: textbody,
+          }
+        },
+        subject: {
+          charset: 'UTF-8',
+          data: 'Message received from haitianrelief.org',
+        },
+      },
+      source: 'tylerschloesser@gmail.com',
+    });
+    puts 'email sent!';
+  rescue => e
+    print "Email not sent. Error message: "
+    puts e
+  end
+
   200
 end
