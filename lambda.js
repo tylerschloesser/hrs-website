e66901c196ea400c71457f9d401c3cbddba46764
Tyler Schloesser <tschloes@amazon.com>
2017-12-26 21:33:27 -0800

    lamdba send email

--- lambda.js
+++ lambda.js
@@ -1,6 +1,32 @@
 const AWS = require('aws-sdk');
 
+AWS.config.update({region: 'us-west-2'});
+
 exports.handler = (event, context, callback) => {
-    // TODO implement
-    callback(null, 'Hello from Lambda');
+
+  const { email, subject, message } = event.body;
+
+  const params = {
+    Destination: {
+      ToAddresses: [ email ]
+    },
+    Message: {
+      Body: {
+        Text: {
+         Charset: "UTF-8",
+         Data: message
+        }
+       },
+       Subject: {
+        Charset: 'UTF-8',
+        Data: subject
+       }
+      },
+    Source: 'tylerschloesser@gmail.com'
+  };
+
+	const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+  sendPromise.then(() => callback()).catch((err) => callback(err))
 };
+
