b3268c9a6cb1e2b081745c194b3098f7c0e49e71
Tyler Schloesser <tschloes@amazon.com>
2021-06-26 13:18:30 -0700

    add mustache loader

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,7 +1,16 @@
 module.exports = {
+  mode: 'development',
   entry: './v1/index.mustache',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mustache/,
+        loader: 'mustache-loader',
+      },
+    ],
   },
 }
