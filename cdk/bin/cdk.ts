1f0a5903b08e28a00130bb9b2610805d791d7a9c
Tyler Schloesser <tschloes@amazon.com>
2021-06-26 11:47:16 -0700

    Add cloudfront distribution

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,7 +4,7 @@ import * as cdk from 'monocdk'
 import { CdkStack } from '../lib/cdk-stack'
 
 const app = new cdk.App()
-new CdkStack(app, 'HrsWebsiteStack', {
+new CdkStack(app, 'OrgHaitianReliefDev', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,18 +1,49 @@
 import * as cdk from 'monocdk'
-import { PublicHostedZone } from 'monocdk/lib/aws-route53'
+import {
+  PublicHostedZone,
+  ARecord,
+  RecordTarget,
+} from 'monocdk/lib/aws-route53'
+import {
+  Certificate,
+  CertificateValidation,
+} from 'monocdk/lib/aws-certificatemanager'
+import { Distribution, ViewerProtocolPolicy } from 'monocdk/lib/aws-cloudfront'
+import { S3Origin } from 'monocdk/lib/aws-cloudfront-origins'
+import { CloudFrontTarget } from 'monocdk/lib/aws-route53-targets'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
+    const domainName = 'dev.haitianrelief.org'
+
     const bucket = new cdk.aws_s3.Bucket(this, 'Bucket', {
-      bucketName: 'dev.haitianrelief.org',
+      bucketName: domainName.split('.').reverse().join('.'),
     })
 
     const hostedZone = new PublicHostedZone(this, 'HostedZone', {
-      zoneName: 'dev.haitianrelief.org',
+      zoneName: domainName,
     })
 
-    // The code that defines your stack goes here
+    const certificate = new Certificate(this, 'Certificate', {
+      domainName,
+      validation: CertificateValidation.fromDns(hostedZone),
+    })
+
+    const distribution = new Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new S3Origin(bucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      domainNames: [domainName],
+      certificate,
+    })
+
+    new ARecord(this, 'AliasRecord', {
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    })
   }
 }
