28509c25c19816059a4527698f9bb83213c4c6e7
Tyler Schloesser <tschloes@amazon.com>
2021-06-26 11:10:46 -0700

    add bucket and hosted zone

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,17 @@
 import * as cdk from 'monocdk'
+import { PublicHostedZone } from 'monocdk/lib/aws-route53'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    const bucket = new cdk.aws_s3.Bucket(this, 'Bucket', {
+      bucketName: 'dev.haitianrelief.org',
+    })
+
+    const hostedZone = new PublicHostedZone(this, 'HostedZone', {
+      zoneName: 'dev.haitianrelief.org',
+    })
 
     // The code that defines your stack goes here
   }
