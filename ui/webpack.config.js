f4a25643a7434eaa083d7ab91c2df4c2c9676ee9
Tyler Schloesser <tschloes@amazon.com>
2021-06-26 19:12:46 -0700

    move public

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,11 +1,13 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
