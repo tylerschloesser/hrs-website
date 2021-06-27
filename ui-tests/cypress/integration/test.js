5e3fff442c99ba86e984d02a0dc15d3008e28932
Tyler Schloesser <tschloes@amazon.com>
2021-06-26 19:42:27 -0700

    update workflow

--- ui-tests/cypress/integration/test.js
+++ ui-tests/cypress/integration/test.js
@@ -1,6 +1,6 @@
 describe('staging.haitianrelief.org', () => {
   it('works', () => {
     cy.visit('https://staging.haitianrelief.org')
-    cy.contains('hello world!')
+    cy.contains('Haitian Relief Services')
   })
 })
